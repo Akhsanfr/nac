97016ffbdb4a7e9c47b490224ef219a2ba1b43d3
Akhsanfr <41402127+Akhsanfr@users.noreply.github.com>
2021-07-26 15:56:25 +0700

    Add files via upload

---
+++ data-panitia.js
@@ -0,0 +1,135 @@
+panitia = {panitia :[
+
+    {
+      nama : 'DIVISI ACARA',
+      staf : [
+        { 
+          id: 1,
+          nama:'Staf Acara Lomba',
+          show:false,
+          tugas:'Membuat konsep, tema, dan melaksanakan teknis serangkaian kegiatan lomba.',
+          formasi: 8,
+          fungsi: [
+            'Menyusun konsep perlombaan',
+            'Menyusun rangkaian kegiatan selama perlombaan,',
+            'Menyusun peraturan dan ketentuan lomba.'
+          ],
+        },
+        { 
+          id: 2,
+          nama:'Staf Acara Seminar',
+          show:false,
+          tugas:'Menyusun konsep, tema, menentukan pembicara serta melaksanakan teknis rangkaian kegiatan seminar.',
+          formasi: 4,
+          fungsi: [
+            'Menentukan tema,',
+            'Menyusun konsep seminar,',
+            'Menentukan narasumber,',
+            'Menyusun rangkaian kegiatan seminar.'
+          ],
+        },
+        { 
+          id: 3,
+          nama:'Staf Acara Pre-Event',
+          show:false,
+          tugas:'Menyusun kegiatan, konsep, tema, serta melaksanakan teknis rangkaian kegiatan Pre-Event.',
+          formasi: 4,
+          fungsi: [
+            'Menentukan rangkaian kegiatan Pre-Event,',
+            'Menyusun konsep kegiatan,',
+            'Menentukan teknis.'
+          ],
+        },
+      ],
+    },
+
+    {
+      nama : 'DIVISI REGISTRASI DAN LIAISON OFFICER (LO)',
+      staf : [
+        { 
+          id: 1,
+          nama:'Staf Media Partner',
+          show:false,
+          tugas:'Berkoordinasi dengan pihak ketiga berupa media partner sebagai sarana publikasi acara.',
+          formasi: 4,
+          fungsi: [
+            'Mencari media publikasi acara,',
+            'Berkoordinasi serta berkomunikasi dengan pihak bersangkutan.'
+          ],
+        },
+        {
+          id: 2,
+          nama:'Staf Registrasi',
+          show:false,
+          tugas:'Membuat konsep dan sistem registrasi peserta dalam rangkaian acara baik lomba maupun seminar',
+          formasi: 4,
+          fungsi: [
+            'Membuat sistem registrasi yang efektif dan efisien,',
+            'Melaksanakan proses registrasi lomba dan seminar,',
+            'Kontrol proses registrasi.'
+          ],
+        },
+        { 
+          id: 3,
+          nama:'Staf Liaison Officer (LO)',
+          show:false,
+          tugas:'Menghubungkan panitia dengan pihak ketiga yang berhubungan dengan acara.',
+          formasi: 8,
+          fungsi: [
+            'Berkoordinasi dengan pihak ketiga berupa narasumber, juri, dan pihak eksternal lain yang berhubungan dengan acara.'
+          ],
+        },
+        { 
+          id: 4,
+          nama:'Staf Sponsorship',
+          show:false,
+          tugas:'Menghubungkan panitia dengan pihak ketiga yang berhubungan dengan sponsor.',
+          formasi: 4,
+          fungsi: [
+            'Survey dan mendata pihak ketiga yang berpotensi sebagai sponsor,',
+            'Berkoordinasi dengan pihak ketiga berupa penyedia sponsor.'
+          ],
+        },
+      ],
+    },
+    {
+      nama : 'DIVISI PUBLIKASI, DESAIN, DAN DOKUMENTASI',
+      staf : [
+        { 
+          id: 1,
+          nama:'Staf Publikasi, Design, dan Dokumentasi (PDD)',
+          show:false,
+          tugas:'Menyusun design untuk publikasi acara di media sosial serta mendokumentasikan rangkaian acara.',
+          formasi: 8,
+          fungsi: [
+            'Melaksanakan dokumentasi pada serangkaian acara',
+            'Melaksanakan publikasi',
+            'Membuat desain yang diperlukan selama acara.'
+          ],
+        },
+        { 
+          id: 2,
+          nama:'Staf Web Developer',
+          show:false,
+          tugas:'Menyusun design dan sistem dari Website NAC.',
+          formasi: 4,
+          fungsi: [
+            'Membuat desain website NAC,',
+            'Menyusun sistem dan teknis website,'
+          ],
+        },
+        { 
+          id: 3,
+          nama:'Staf Media Streamer',
+          show:false,
+          tugas:'Bertanggung jawab atas room yang digunakan selama rangkaian kegiatan serta publikasi ke media sosial.',
+          formasi: 4,
+          fungsi: [
+            'Menjadi operator room saat pelaksana acara,',
+            'Melakukan streaming jika diperlukan.'
+          ],
+        }
+      ],
+    }
+
+  ]}
